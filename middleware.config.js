39f46287ae2c3dabf3025bad43b7cf4a06a6283c
BMonkey <86775351+BS-Monkey@users.noreply.github.com>
2022-04-22 13:38:09 +0300

    my first vue store frontend page
    using vue.js, vue store frontend component

---
+++ middleware.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  integrations: {
+    sb: {
+      location: '@vue-storefront/storyblok/server',
+      configuration: {
+        token: 'PV0kSyyd3U2DwQYkAtkxIQtt-112836-4ma7hGGKERHYu_ee-Mvg',
+        cacheProvider: 'memory',
+      },
+    }
+  }
+};
